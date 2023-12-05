0c3cea01ea340cfe11663b852a0942a7eb65b059
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// function shortcut(s1, s2) {
+//   // your code here
+// }
+
+// // Do not change the code below.
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
+
+
 function shortcut(s1, s2) {
-  // your code here
+  const commonCharacters = Array.from(new Set([...s1].filter(char => s2.includes(char))));
+  return commonCharacters.join('');
 }
 
-// Do not change the code below.
+// Example
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
